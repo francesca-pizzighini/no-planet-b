3ef0b6a5766893cee272cb401978bcdfe4a4a90d
francesca-pizzighini <133757414+francesca-pizzighini@users.noreply.github.com>
2024-03-18 15:51:56 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from "./assets/img/logo.png"
+
+function App() {
+
+  return (
+    <>
+      home page
+      <img src={logo} alt="logo" />
+    </>
+  )
+}
+
+export default App
